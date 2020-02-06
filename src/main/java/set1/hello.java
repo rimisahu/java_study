7dd2e95cb7c0fc2bfdac737f90ff3aaa6dc4e50e
rs <rimi.m.sahu@gmail.com>
2020-02-05 21:56:41 -0800

    ints

--- src/main/java/set1/hello.java
+++ src/main/java/set1/hello.java
@@ -4,7 +4,14 @@ public class hello {
 
     public static void main(String[] args){
 
+        //print statement
         System.out.println("hello world");
+
+        //setting integers
+        int first_integer=0;
+        System.out.println(first_integer);
+
+
 
     }
 }
