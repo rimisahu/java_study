023a3ce6a632c3b6d705d4d9ffc1fdf290d8470a
rs <rimi.m.sahu@gmail.com>
2020-03-21 16:50:19 -0700

    operators test

--- src/main/java/set1/operators_test.java
+++ src/main/java/set1/operators_test.java
@@ -4,5 +4,35 @@ public class operators_test {
 
     public static void main(String[] args){
 
+        /*
+        list of java operators:
+            Unary Operator,
+            Arithmetic Operator,
+            Shift Operator,
+            Relational Operator,
+            Bitwise Operator,
+            Logical Operator,
+            Ternary Operator and.
+            Assignment Operator.
+
+         */
+
+        int test_value=10;
+        System.out.println("addition: " + (test_value += 2));
+        System.out.println("subtraction: "+ (test_value -= 2));
+        System.out.println("multiplication: "+ (test_value *= 2));
+
+
+        //------------------------------
+
+        //operator precedence
+        //ternary operator
+
+        //(5==5) --> check if the condition is true
+        //true --> if condition ^ is true, then choose this
+        //false --> if condition ^ is false, then choose this
+        boolean result= (5==5) ? true : false;
+        System.out.println("\nternary operator result: "+result);
+
     }
 }
